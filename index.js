e9ce69c5b6df182b01529d481439b1524d538f1f
TSAELENS <77736077+TSAELENS@users.noreply.github.com>
2024-02-15 09:47:15 +0100

    Configuration Thread et Sender

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const mysql = require('mysql');
+const role = process.env.ROLE; // 'sender' ou 'thread'
 
 // Créez une connexion à la base de données
 // Wamp
@@ -37,34 +38,43 @@ app.use(express.json());
 // Configuration pour servir des fichiers statiques depuis le répertoire "public"
 app.use(express.static('public'));
 
-
-// Route pour afficher les messages
-app.get('/messages', (req, res) => {
-  db.query('SELECT pseudo, content, timestamp FROM messages', (err, rows) => {
-    if (err) {
-      console.error('Erreur lors de la récupération des messages:', err);
-      return res.status(500).json({ error: 'Erreur lors de la récupération des messages' });
-    }
-    res.json(rows);
+if (role === 'thread') {
+  // Route pour afficher les messages
+  app.get('/messages', (req, res) => {
+    db.query('SELECT pseudo, content, timestamp FROM messages', (err, rows) => {
+      if (err) {
+        console.error('Erreur lors de la récupération des messages:', err);
+        return res.status(500).json({ error: 'Erreur lors de la récupération des messages' });
+      }
+      res.json(rows);
+    });
   });
-});
 
-// Route pour poster un message
-app.post('/messages', (req, res) => {
-  const { pseudo, content } = req.body;
-  if (!pseudo || !content) {
-    return res.status(400).json({ error: 'Le pseudo et le contenu du message sont requis' });
-  }
-  const newMessage = { pseudo, content};
-
-  db.query('INSERT INTO messages SET ?', newMessage, (err, result) => {
-    if (err) {
-      console.error('Erreur lors de l\'insertion du message:', err);
-      return res.status(500).json({ error: 'Erreur lors de l\'insertion du message' });
-    }
-    res.status(201).json(newMessage);
+  // Route pour servir le fichier index.html
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
   });
-});
+
+}
+
+if (role === 'sender') {
+  // Route pour poster un message
+  app.post('/messages', (req, res) => {
+    const { pseudo, content } = req.body;
+    if (!pseudo || !content) {
+      return res.status(400).json({ error: 'Le pseudo et le contenu du message sont requis' });
+    }
+    const newMessage = { pseudo, content};
+
+    db.query('INSERT INTO messages SET ?', newMessage, (err, result) => {
+      if (err) {
+        console.error('Erreur lors de l\'insertion du message:', err);
+        return res.status(500).json({ error: 'Erreur lors de l\'insertion du message' });
+      }
+      res.status(201).json(newMessage);
+    });
+  });
+}
 
 // Gestion des erreurs
 app.use((err, req, res, next) => {
@@ -72,11 +82,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Une erreur interne est survenue' });
 });
 
-// Route pour servir le fichier index.html
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-  });
-  
+
 
 
 // Démarrage du serveur
